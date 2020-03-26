ff8130cd80413e5c3f4432246d88325e7d343087
isaac039 <62724250+isaac039@users.noreply.github.com>
2020-03-26 17:20:43 -0400

    Add files via upload

---
+++ FavoritesWeb/main.js
@@ -0,0 +1,36 @@
+let viewZi = document.getElementById('zidaneopen');
+let hideZi = document.getElementById('hidezidane');
+
+
+hideZi.hidden = true;
+
+viewZi.onclick = function() {
+  viewZi.hidden = true;
+  hideZi.hidden = false;
+}
+
+const ffixFactory = (name, age, quote) => {
+  return {
+    name: name,
+    age: age,
+    quote: quote;
+  }
+};
+
+const tZidane = ffixFactory('Zidane Tribal', 16, 'You don\'t need a reason to help people.');
+}
+const aGarnet = ffixFactory('Garnet Til Alexandros XVII', 16, 'Someday I will be queen, but I will always be myself.');
+
+const oVivi = ffixFactory('Vivi Ornitier', 9, 'How do you prove that you exist...? Maybe we don\'t exist...');
+
+const aSteiner = ffixFactory('Adelbert Steiner', 33, 'Having sworn fealty, must I spend my life in servitude?');
+
+const cAmarant = ffixFactory('Amarant Coral', 26, 'The only dependable thing about the future is uncertainty.');
+
+const cFreya = ffixFactory('Freya Crescent', 21, 'To be forgotten is worse than death.');
+
+const qQuina = ffixFactory('Quina Quen', 89, 'I do what I want! You have problem!?');
+
+const cEiko = ffixFactory('Eiko Carol', 6, 'I don\'t wanna be alone anymore...');
+
+const tKuja = ffixFactory('Kuja', 24, 'The weak lose their freedom to the strong. Such is the way of the strong. And it is the providence of nature that only the strong survive. That is why I needed strength.');
